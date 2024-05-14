af915569d6af197ff77734ab15f3553c731aaf60
Mica <mika89700@gmail.com>
2024-05-14 21:51:15 +0200

    use arduino lint

--- 7_SEG/dataFile.h
+++ 7_SEG/dataFile.h
@@ -11,19 +11,19 @@
 #define STEPS_FOLDER "/data/steps"
 
 class FSData {
-	public:
-		String content;
-		std::vector< String > files;
+    public:
+        String content;
+        std::vector< String > files;
 
-	public:
-		static void listDir(fs::FS &fs, const char * dirname);
-		static void createDir(fs::FS &fs, const char * path);
-		static void removeDir(fs::FS &fs, const char * path);
-		static void readFile(fs::FS &fs, const char * path);
-		static void writeFile(fs::FS &fs, const char * path, const char * message);
-		static void appendFile(fs::FS &fs, const char * path, const char * message);
-		static void renameFile(fs::FS &fs, const char * path1, const char * path2);
-		static void deleteFile(fs::FS &fs, const char * path);
+    public:
+        void listDir(fs::FS &fs, const char * dirname);
+        static void createDir(fs::FS &fs, const char * path);
+        static void removeDir(fs::FS &fs, const char * path);
+        void readFile(fs::FS &fs, const char * path);
+        static void writeFile(fs::FS &fs, const char * path, const char * message);
+        static void appendFile(fs::FS &fs, const char * path, const char * message);
+        static void renameFile(fs::FS &fs, const char * path1, const char * path2);
+        static void deleteFile(fs::FS &fs, const char * path);
 };
 
 #endif
